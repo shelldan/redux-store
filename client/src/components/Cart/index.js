fe4ca84272ceb04deb6544a9eb2b4cfdeb448e2a
shelldan <shelldanlee@gmail.com>
2023-01-31 18:19:08 -0600

    refactor Cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,20 +1,34 @@
+// import react dependencies 
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+
+// import stripe dependency 
 import { loadStripe } from '@stripe/stripe-js';
+
+// import apollo dependency 
 import { useLazyQuery } from '@apollo/client';
+
+// import utils dependencies 
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
+//import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+
+// import component 
+import CartItem from '../CartItem';
+
+// import style 
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch(); 
+  const state = useSelector(state => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
+  // if data is updated, and data object exists, redirect to checkout referencing sessionId
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
@@ -23,6 +37,7 @@ const Cart = () => {
     }
   }, [data]);
 
+  // if cart.length or dispatch is updated, get items from session and populate cart if it is empty 
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
@@ -34,10 +49,12 @@ const Cart = () => {
     }
   }, [state.cart.length, dispatch]);
 
+  // toggle cart when cart icon is clicked
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
 
+  // loop through each item in cart, multiply item.price by the item.purchaseQuantity, and then add to the sum
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach((item) => {
@@ -46,6 +63,7 @@ const Cart = () => {
     return sum.toFixed(2);
   }
 
+  // loop through each item in cart and add item._id to productIds array, which is returned from query
   function submitCheckout() {
     const productIds = [];
 
